5a5cc4dd5e3170f6674d161f06d26653809841c9
Ryan Mathisen and Ryan Peterson <ryan+ryan@>
2017-01-25 16:40:43 -0800

    almost implement a working SomeFunction(), finished function TBD

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const MINE = 9;
+
 @Component({
   selector: 'board',
   templateUrl: './board.component.html',
@@ -8,8 +10,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BoardComponent implements OnInit {
   @Input() board: number[][];
   @Input() revealed: boolean[][];
+  @Input() boardWidth: number;
 
   showStyle: true;
+  toCheck: number[][];
+
   constructor() { }
 
   ngOnInit() {
@@ -23,4 +28,28 @@ export class BoardComponent implements OnInit {
     this.revealed[i][j] = true;
   }
 
+  prepareToCheck(i: number, j: number) {
+    var array = [i, j];
+    this.toCheck.push(array);
+  }
+
+  someFunction(i: number, j: number) {
+    this.toCheck = [];
+    if (this.board[i][j] != MINE && this.board[i][j] === 0) {
+      for (var x = -1; x < 2; x++) {
+        for (var y = -1; y < 2; y++) {
+          if (  i + x >= 0 &&
+            j + y >= 0 &&
+            i + x < this.boardWidth &&
+            j + y < this.boardWidth) {
+              this.prepareToCheck(i+x, j+y);
+          }
+        }
+      }
+    }
+    this.toCheck.forEach(function(elem) {
+      this.revealSquare(elem[0], elem[1]);
+    })
+
+  }
 }
